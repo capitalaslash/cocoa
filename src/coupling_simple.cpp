e53fdc2e21b5076e9748251f4ffc56e6064df089
Antonio Cervone <ant.cervone@gmail.com>
2024-09-13 15:32:54 +0200

    interpolate P1P1 in CouplingSimple

--- src/coupling_simple.cpp
+++ src/coupling_simple.cpp
@@ -19,37 +19,46 @@ void CouplingSimple::setup(Problem * pSrc, Problem * pTgt)
   pSrc_ = pSrc;
   pTgt_ = pTgt;
 
-  m_.init(derTgt->n_ - 1, derSrc->n_ - 1);
-  for (uint row = 0; row < derTgt->n_ - 1; row++)
+  // P1P1
+  m_.init(derTgt->n_, derSrc->n_);
+  for (uint row = 0; row < derTgt->n_; row++)
   {
-    auto const startTgt = derTgt->start_ + row * derTgt->h_;
-    auto const endTgt = startTgt + derTgt->h_;
-    for (uint clm = 0; clm < derSrc->n_ - 1; clm++)
+    auto const ptTgt = derTgt->start_ + row * derTgt->h_;
+    for (uint clm = 0; clm < derSrc->n_; clm++)
     {
       auto const startSrc = derSrc->start_ + clm * derSrc->h_;
       auto const endSrc = startSrc + derSrc->h_;
-      if (((startSrc >= startTgt - 1.e-12) && startSrc < (endTgt + 1.e-12)) ||
-          ((endSrc >= startTgt - 1.e-12) && (endSrc < endTgt + 1.e-12)))
+      if (ptTgt >= startSrc && ptTgt < endSrc)
       {
-        double const start = std::max(startTgt, startSrc);
-        double const end = std::min(endTgt, endSrc);
-        double const frac = (end - start) / derSrc->h_;
-        m_(row, clm) = frac;
+        m_(row, clm) = (endSrc - ptTgt) / (endSrc - startSrc);
+        m_(row, clm + 1) = 1. - m_(row, clm);
+        break;
       }
     }
   }
-  fmt::print("m: {} x {}\n{:::.2e}\n", m_.data.size(), m_.data[0].size(), m_.data);
 
-  // std::vector<double> fieldSrc(derSrc->n_ - 1, 1.0);
-  // std::vector<double> fieldTgt(derTgt->n_ - 1, 0.0);
-  // for (uint i = 0; i < fieldTgt.size(); i++)
+  // P0P0
+  // m_.init(derTgt->n_ - 1, derSrc->n_ - 1);
+  // for (uint row = 0; row < derTgt->n_ - 1; row++)
   // {
-  //   for (uint j = 0; j < fieldSrc.size(); j++)
+  //   auto const startTgt = derTgt->start_ + row * derTgt->h_;
+  //   auto const endTgt = startTgt + derTgt->h_;
+  //   for (uint clm = 0; clm < derSrc->n_ - 1; clm++)
   //   {
-  //     fieldTgt[i] += m_(i, j) * fieldSrc[j];
+  //     auto const startSrc = derSrc->start_ + clm * derSrc->h_;
+  //     auto const endSrc = startSrc + derSrc->h_;
+  //     if (((startSrc >= startTgt - 1.e-12) && startSrc < (endTgt + 1.e-12)) ||
+  //         ((endSrc >= startTgt - 1.e-12) && (endSrc < endTgt + 1.e-12)))
+  //     {
+  //       double const start = std::max(startTgt, startSrc);
+  //       double const end = std::min(endTgt, endSrc);
+  //       double const frac = (end - start) / derSrc->h_;
+  //       m_(row, clm) = frac;
+  //     }
   //   }
   // }
-  // fmt::print("fieldTgt: {::.1e}\n", fieldTgt);
+
+  // fmt::print("m: {} x {}\n{:::.2e}\n", m_.data.size(), m_.data[0].size(), m_.data);
 }
 
 void CouplingSimple::project(
@@ -58,32 +67,45 @@ void CouplingSimple::project(
   auto derSrc = dynamic_cast<ProblemFD1D *>(pSrc_);
   auto derTgt = dynamic_cast<ProblemFD1D *>(pTgt_);
 
-  // convert P1 to P0
+  // interpolate P1P1
   FieldCoupling * p1Src = derSrc->getField(fieldNameSrc);
-  std::vector<double> p0Src(p1Src->size() - 1);
-  for (uint k = 0; k < p0Src.size(); k++)
-  {
-    p0Src[k] = 0.5 * (*(p1Src->dataPtr() + k) + *(p1Src->dataPtr() + k + 1));
-  }
-
-  // interpolate
   FieldCoupling * p1Tgt = derTgt->getField(fieldNameTgt);
-  std::vector<double> p0Tgt(p1Tgt->size() - 1);
-  for (uint i = 0; i < p0Tgt.size(); i++)
+  std::vector<double> dataTgt(p1Tgt->size());
+  for (uint i = 0; i < p1Tgt->size(); i++)
   {
-    for (uint j = 0; j < p0Src.size(); j++)
+    for (uint j = 0; j < p1Src->size(); j++)
     {
-      p0Tgt[i] += m_(i, j) * p0Src[j];
+      dataTgt[i] += m_(i, j) * *(p1Src->dataPtr() + j);
     }
   }
+  p1Tgt->setValues(dataTgt);
 
-  // convert back P0 to P1
-  std::vector<double> p1TgtData(p1Tgt->size());
-  p1TgtData[0] = p0Tgt[0];
-  p1TgtData[p1Tgt->size() - 1] = p0Tgt[p0Tgt.size() - 1];
-  for (uint k = 1; k < p1Tgt->size() - 1; k++)
-  {
-    p1TgtData[k] = 0.5 * (p0Tgt[k - 1] + p0Tgt[k]);
-  }
-  p1Tgt->setValues(p1TgtData);
+  // // convert P1 to P0
+  // FieldCoupling * p1Src = derSrc->getField(fieldNameSrc);
+  // std::vector<double> p0Src(p1Src->size() - 1);
+  // for (uint k = 0; k < p0Src.size(); k++)
+  // {
+  //   p0Src[k] = 0.5 * (*(p1Src->dataPtr() + k) + *(p1Src->dataPtr() + k + 1));
+  // }
+
+  // // interpolate P0P0
+  // FieldCoupling * p1Tgt = derTgt->getField(fieldNameTgt);
+  // std::vector<double> p0Tgt(p1Tgt->size() - 1);
+  // for (uint i = 0; i < p0Tgt.size(); i++)
+  // {
+  //   for (uint j = 0; j < p0Src.size(); j++)
+  //   {
+  //     p0Tgt[i] += m_(i, j) * p0Src[j];
+  //   }
+  // }
+
+  // // convert back P0 to P1
+  // std::vector<double> p1TgtData(p1Tgt->size());
+  // p1TgtData[0] = p0Tgt[0];
+  // p1TgtData[p1Tgt->size() - 1] = p0Tgt[p0Tgt.size() - 1];
+  // for (uint k = 1; k < p1Tgt->size() - 1; k++)
+  // {
+  //   p1TgtData[k] = 0.5 * (p0Tgt[k - 1] + p0Tgt[k]);
+  // }
+  // p1Tgt->setValues(p1TgtData);
 }
